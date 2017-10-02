d2e6f3da267328416d153ae6ab38dd38245d2a60
Nicolas Gleiser <ncs.cr4sh@gmail.com>
2017-10-01 22:51:11 -0300

    Feito

--- my-cookbook/config/routes.rb
+++ my-cookbook/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :home
   resources :recipes
+  resources :cuisines
 end
--- my-cookbook/spec/controllers/cuisine_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CuisineController, type: :controller do
-
-end
