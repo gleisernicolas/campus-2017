46eabe0e94801549feb4fd2f39d32d80e8713838
Nicolas Gleiser <ncs.cr4sh@gmail.com>
2017-10-01 23:30:11 -0300

    Feito

--- my-cookbook/app/controllers/cuisines_controller.rb
+++ my-cookbook/app/controllers/cuisines_controller.rb
@@ -11,4 +11,16 @@ class CuisinesController < ApplicationController
     @cuisine = Cuisine.all
   end
 
+  def create
+    @cuisine = Cuisine.new(params.require(:cuisine).permit(:name))
+
+    if @cuisine.save
+      redirect_to cuisine_url @cuisine
+    else
+
+      flash[:error]= 'Você deve informar o nome da cozinha'
+      redirect_to new_cuisine_url
+    end
+  end
+
 end
